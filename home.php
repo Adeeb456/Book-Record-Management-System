29cb2e6dd81793d04f8e62dbf9ed2ccfcd19b9e5
Adeeb Khan <adeeb@DESKTOP-OASBFC7.localdomain>
2022-01-31 07:47:09 +0530

    Home.php file is added. It will render the homepage of BRMS

---
+++ home.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home.css">
+    <title>Home Page</title>
+</head>
+
+<body>
+
+    <div id="container">
+
+        <h1> Book Record Management System </h1>
+        <hr>
+
+        <br>
+        <br>
+        <br>
+
+        <div class="LinkDiv">
+
+            <a href="InsertForm.php"> Insert Books </a>
+
+        </div>
+
+        <br>
+        <br>
+
+
+        <div class="LinkDiv">
+
+            <a href="DeleteForm.php"> Delete Books </a>
+
+        </div>
+
+
+        <br>
+        <br>
+
+        <div class="LinkDiv">
+
+            <a href="view.php"> View Cart Stock </a>
+
+        </div>
+
+        <br>
+        <br>
+
+        <div class="LinkDiv">
+
+            <a href="update.php"> Update Cart Stock </a>
+        </div>
+
+
+    </div>
+
+</body>
+
+</html>
